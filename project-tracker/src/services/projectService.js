1dd7a49fb511f375f75171a66ed1a9f646dec526
Taquispro <121485751+Taquispro@users.noreply.github.com>
2026-01-19 03:22:02 +0530

    Update projectService.js

--- project-tracker/src/services/projectService.js
+++ project-tracker/src/services/projectService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080";
+const API_URL = "https://projecttrackerjar.onrender.com/";
 
 export const getProjects = () => {
   let result = axios.get(`${API_URL}/projects`);
