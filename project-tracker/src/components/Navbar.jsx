6ca96efd38b40eb8e8dca31db11db95fc0b04ede
Taquispro <121485751+Taquispro@users.noreply.github.com>
2026-02-08 05:43:01 +0530

    Update Navbar.jsx

--- project-tracker/src/components/Navbar.jsx
+++ project-tracker/src/components/Navbar.jsx
@@ -1,9 +1,27 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
-export default function Navbar() {
+export default function Navbar({ searchBox }) {
+  const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    const text = e.target.value;
+    setValue(text);
+    searchBox(e); // send event to App.jsx
+  };
+
   return (
     <nav style={styles.nav}>
       <h3 style={styles.logo}>Project Tracker</h3>
+
+      <input
+        type="text"
+        value={value}
+        onChange={handleChange}
+        placeholder="Search projects..."
+        style={styles.input}
+      />
+
       <div style={styles.links}>
         <Link to="/" style={styles.link}>
           Projects
@@ -29,6 +47,13 @@ const styles = {
     margin: 0,
     fontSize: "18px",
     fontWeight: "600",
+  },
+  input: {
+    padding: "8px 12px",
+    borderRadius: "6px",
+    border: "1px solid #374151",
+    outline: "none",
+    width: "260px",
   },
   links: {
     display: "flex",
